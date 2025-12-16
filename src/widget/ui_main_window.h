39d74c31107d4e51a5efda9c969fbca2ee1fdaba
wangshijie <3116517662@qq.com>
2025-12-16 20:32:21 +0800

    设计初始界面框架

--- src/widget/ui_main_window.h
+++ src/widget/ui_main_window.h
@@ -11,8 +11,6 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QLabel>
-#include <QtWidgets/QStackedWidget>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -20,45 +18,14 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QStackedWidget *stackedWidget;
-    QWidget *page;
-    QLabel *label_3;
-    QWidget *page_3;
-    QLabel *label;
-    QWidget *page_2;
-    QLabel *label_2;
 
     void setupUi(QWidget *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(400, 300);
-        stackedWidget = new QStackedWidget(MainWindow);
-        stackedWidget->setObjectName(QString::fromUtf8("stackedWidget"));
-        stackedWidget->setGeometry(QRect(0, 0, 401, 301));
-        page = new QWidget();
-        page->setObjectName(QString::fromUtf8("page"));
-        label_3 = new QLabel(page);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setGeometry(QRect(30, 40, 54, 12));
-        stackedWidget->addWidget(page);
-        page_3 = new QWidget();
-        page_3->setObjectName(QString::fromUtf8("page_3"));
-        label = new QLabel(page_3);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(0, 0, 141, 61));
-        stackedWidget->addWidget(page_3);
-        page_2 = new QWidget();
-        page_2->setObjectName(QString::fromUtf8("page_2"));
-        label_2 = new QLabel(page_2);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setGeometry(QRect(0, 0, 54, 41));
-        stackedWidget->addWidget(page_2);
 
         retranslateUi(MainWindow);
-
-        stackedWidget->setCurrentIndex(0);
-
 
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
@@ -66,9 +33,6 @@ public:
     void retranslateUi(QWidget *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Form", nullptr));
-        label_3->setText(QCoreApplication::translate("MainWindow", "\347\254\254\344\270\211\351\241\265", nullptr));
-        label->setText(QCoreApplication::translate("MainWindow", "\347\254\254\344\270\200\351\241\265", nullptr));
-        label_2->setText(QCoreApplication::translate("MainWindow", "\347\254\254\344\272\214\351\241\265", nullptr));
     } // retranslateUi
 
 };
