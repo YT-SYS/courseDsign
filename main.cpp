cd41977eafd6586d19dc742390fd4586293a82f2
wangshijie <3116517662@qq.com>
2025-12-17 19:44:43 +0800

    临时

--- main.cpp
+++ main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
+#include <QWidget>
 #include <QApplication>
 #include <vector>
 #include <QPushButton>
 #include "in_manager.h"
+#include "ui_main_window.h"
+
+
+class Widget : public QWidget{
+public:
+    Widget(QWidget *parent);
+    ~Widget() override;
+
+private:
+    Ui_MainWindow *ui;
+};
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    ui = new Ui_MainWindow;
+    ui->setupUi(this);
+}
+Widget::~Widget() {
+
+}
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     QWidget widget(nullptr);
-    widget.resize(800,600);
-    QPushButton button(&widget);
-    button.setText("hello world");
-    widget.show();
-
-    InputManger  in_manager_;
+    Widget widget1(nullptr);
+    widget1.show();
 
     return a.exec();
 }
